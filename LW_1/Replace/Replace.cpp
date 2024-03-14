e237b741e571cc07a73db7d478281cb9aaab0b45
Dmitriy <mitya.lyashko.02@mail.ru>
2024-03-14 13:06:46 +0300

    Рефакторинг Необходимо: - исправить .bat файл (сейчас он ничего не проверяет) - переделать функцию замены строки, убрать двойную вложенность - добавить errorlevel с разными значениями

--- LW_1/Replace/Replace.cpp
+++ LW_1/Replace/Replace.cpp
@@ -3,7 +3,18 @@
 #include <string>
 using namespace std;
 
-string replaceString(string str, string oldWord, string newWord) {
+const string ARGUMENT_COUNT_ERROR = "Invalid argument count!";
+const string INPUT_OPEN_ERROR = "Error on opening input-file";
+const string OUTPUT_OPEN_ERROR = "Error on opening output-file";
+const string INPUT_FORMAT = "Input format: replace.exe <input file> <output file> <search string> <replace string>";
+const int SECOND_ARGUMENT = 1;
+const int THIRD_ARGUMENT = 2;
+const int FOURTH_ARGUMENT = 3;
+const int FIVE_ARGUMENT = 4;
+const int REQUIRED_QUANTITY_ARGUMENTS = 5;
+
+string replaceString(string str, string oldWord, string newWord) 
+{
     if (oldWord.length() == 0) return str;
     bool status = false;
     size_t startIndex = 0;
@@ -23,10 +34,7 @@ string replaceString(string str, string oldWord, string newWord) {
                     i = startIndex + 1;
                     break;
                 }
-                else 
-                {
-                    status = true;
-                }
+                status = true;
             }
             if (status) 
             {
@@ -37,29 +45,20 @@ string replaceString(string str, string oldWord, string newWord) {
     return str;
 }
 
-int main(int argc, char* argv[]) {
-    const string invalidArgumentError = "Invalid argument count!";
-    const string inputOpenError = "Error on opening input-file";
-    const string outputOpenError = "Error on opening output-file";
-    const string inputFormat = "Input format: replace.exe <input file> <output file> <search string> <replace string>";
-    const int firstArgument = 1;
-    const int secondArgument = 2;
-    const int thirdArgument = 3;
-    const int fourthArgument = 4;
+int main(int argc, char* argv[])
+{
 
-    const int requiredQuantityArguments = 5;
-
-    if (argc != requiredQuantityArguments)
+    if (argc != REQUIRED_QUANTITY_ARGUMENTS)
     {
-        cerr << invalidArgumentError << endl;
-        cerr << inputFormat << endl;
+        cerr << ARGUMENT_COUNT_ERROR << endl;
+        cerr << INPUT_FORMAT << endl;
         return 1;
     }
 
-    string inputFileName = argv[firstArgument];
-    string ouputFIleName = argv[secondArgument];
-    string searchString = argv[thirdArgument];
-    string replacingString = argv[fourthArgument];
+    string inputFileName = argv[SECOND_ARGUMENT];
+    string ouputFIleName = argv[THIRD_ARGUMENT];
+    string searchString = argv[FOURTH_ARGUMENT];
+    string replacingString = argv[FIVE_ARGUMENT];
     ifstream inf;
     ofstream outf;
 
@@ -67,23 +66,20 @@ int main(int argc, char* argv[]) {
 
     if (!inf.is_open())
     {
-        cerr << inputOpenError << endl;
+        cerr << INPUT_OPEN_ERROR << endl;
         return 1;
     }
-    else 
-    {   
-        outf.open(ouputFIleName);
-        if (!outf.is_open())
-        {
-            cerr << outputOpenError << endl;
-            return 1;
-        }
-        string str;
-        while (getline(inf, str))
-        {
-            str = replaceString(str, searchString, replacingString);
-            outf << str << endl;
-        }
+    outf.open(ouputFIleName);
+    if (!outf.is_open())
+    {
+        cerr << OUTPUT_OPEN_ERROR << endl;
+        return 1;
+    }
+    string str;
+    while (getline(inf, str))
+    {
+        str = replaceString(str, searchString, replacingString);
+        outf << str << endl;
     }
 
     inf.close();
