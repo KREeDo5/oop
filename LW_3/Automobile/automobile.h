a7278bb77fcffc0eb2d08217195b8a4c116e4e53
Dmitrii Liashko <mitya.lyashko.02@mail.ru>
2024-05-16 01:23:46 +0300

    описал setGear

--- LW_3/Automobile/automobile.h
+++ LW_3/Automobile/automobile.h
@@ -7,6 +7,8 @@ const std::string NEUTRAL_SET_SPEED_ERROR = "Включена нейтральная передача. Вращ
 const std::string MIN_SET_SPEED_ERROR = "Скорость слишком мала для текущей передачи, понизьте передачу.";
 const std::string NEGATIVE_SET_SPEED_ERROR = "Скорость не может быть отрицательной. Введите положительное значение.";
 const std::string MAX_SET_SPEED_ERROR = "Скорость слишком высока для текущей передачи, повысьте передачу.";
+const std::string MIN_SET_GEAR_ERROR = "Текущая скорость не подходит для этой передачи. Увеличьте скорость.";
+const std::string MAX_SET_GEAR_ERROR = "Текущая скорость не подходит для этой передачи. Понизьте скорость.";
 
 class Car 
 {
@@ -65,26 +67,44 @@ class Car
             return false;
         }
 
-        //TODO: реализовать подход с использованием массивов
         bool setGear(int gear)
-            //Выбрать указанную передачу [-1..5]. В случае успешного переключения передачи
-            // (в том числе и на саму себя) возвращает true.
         {
             // если передачи не существует
             if (gear < gearToInt(Gear::REVERSE) || gear > gearToInt(Gear::FIFTH))
             {
                 return false;
             }
+
             // если передача - задняя, но сейчас скорость больше чем 0
             if (gear == gearToInt(Gear::REVERSE) && currentGear != Gear::REVERSE && currentSpeed > PARKING_SPEED)
             {
                 return false;
             }
-            // если передача 1-5, а сейчас включена задняя и мы движемся - 
+
+            // если передача 1-5, а сейчас включена задняя и мы движемся
             if (gear > gearToInt(Gear::NEUTRAL) && currentGear == Gear::REVERSE && currentSpeed > PARKING_SPEED)
             {
                 return false;
             }
+
+            SpeedLimit speedLimit = getSpeedLimits(Gear(gear));
+            int minSpeed = speedLimit.getMinSpeed();
+            int maxSpeed = speedLimit.getMaxSpeed();
+
+            // если скорость недостаточна для желаемой передачи
+            if (currentSpeed < minSpeed)
+            {   
+                std::cout << MIN_SET_GEAR_ERROR << std::endl;
+                return false;
+            }
+
+            // если скорость велика для желаемой передачи
+            if (currentSpeed > maxSpeed)
+            {
+                std::cout << MAX_SET_GEAR_ERROR << std::endl;
+                return false;
+            }
+
             currentGear = Gear(gear);
             return true;
         }
