84dca24edc695579a518be638fa4352dbe86e6cb
Dmitrii Liashko <mitya.lyashko.02@mail.ru>
2024-05-15 21:56:35 +0300

    Описал геттеры, TODOшки

--- LW_3/Automobile/automobile.h
+++ LW_3/Automobile/automobile.h
@@ -1,63 +1,80 @@
 #pragma once
 
 class Car {
+private:
+    bool engineOn = false; // Состояние двигателя (включен или выключен)
+    int currentGear = 0; // Текущая выбранная передача [-1..5]
+    int currentSpeed = 0; // Текущая скорость движения (целое число от 0 до максимальной скорости) 
 
-    //МЕТОДЫ:
-
-    bool IsTurnedOn() 
-    // Состояние двигателя (включен или выключен)
+public:
+    bool isTurnedOn()
     {
+        return engineOn;
+    }
+
+    int getSpeed()
+    {
+        return currentGear;
+    }
+
+    int getGear()
+    {
+        return currentGear;
+    }
+
+    std::string getDirection()
+    {
+        if (currentSpeed == 0) {
+            return "Стоим на месте";
+        }
+        if (currentGear > 0) {
+            return "Едем вперёд";
+        }
+        return "Едем назад";
+    }
+
+    bool setSpeed(int speed)
+        //Задать указанную скорость. Возвращает true, если скорость удалось изменить 
+        // и false, если изменить скорость движения на указанную невозможно
+        // (например, на нейтральной передаче нельзя разогнаться).
+    {
+        if (currentGear == 0) {
+            return false;
+        }
         return true;
     }
 
-    bool GetDirection()
-    // Направление движения (вперед, назад или стоим на месте)
+
+    //TODO: реализовать подход с использованием массивов
+    bool setGear(int gear)
+        //Выбрать указанную передачу [-1..5]. В случае успешного переключения передачи
+        // (в том числе и на саму себя) возвращает true.
     {
+        if (gear < -1 || gear > 5){
+            return false;
+        }
+        if (gear == -1 && currentGear != -1 && currentSpeed > 0) {
+            return false;
+        }
+        if (gear > 0 && currentGear == -1 && currentSpeed > 0) {
+            return false;
+        }
+        currentGear = gear;
         return true;
     }
 
-    bool GetSpeed()
-    // Текущая скорость движения (целое число от 0 до максимальной скорости) 
+    bool turnOnEngine()
     {
+        engineOn = true;
         return true;
     }
 
-    bool GetGear()
-    // Текущая выбранная передача [-1..5]
+    bool turnOffEngine()
     {
+        if (currentGear != 0 || currentSpeed > 0) {
+            return false;
+        }
+        engineOn = false;
         return true;
-    }
-
-
-    //ФУНКЦИИ:
-
-    bool TurnOnEngine()
-    //Включить двигатель (если он выключен). 
-    // Возвращает true, если двигатель включился (или уже был включен).
-    {
-        std::cout << "hello" << std::endl;
-    }
-
-    bool TurnOffEngine()
-    //Выключить двигатель (если он включен и текущая передача – нейтральная, а автомобиль стоит). 
-    // Возвращает true, если двигатель был успешно выключен (или уже был выключен), 
-    // и false, если двигатель не может быть в данный момент выключен.
-    {
-        std::cout << "hello" << std::endl;
-    }
-
-    bool SetGear(int gear)
-    //Выбрать указанную передачу [-1..5]. В случае успешного переключения передачи
-    // (в том числе и на саму себя) возвращает true.
-    {
-        std::cout << "hello" << std::endl;
-    }
-
-    bool SetSpeed(int speed)
-    //Задать указанную скорость. Возвращает true, если скорость удалось изменить и false,
-    // если изменить скорость движения на указанную невозможно
-    // (например, на нейтральной передаче нельзя разогнаться).
-    {
-        std::cout << "hello" << std::endl;
     }
 };
