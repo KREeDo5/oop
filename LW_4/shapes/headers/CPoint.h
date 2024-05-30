20f2ff2739d2294eca5d612ea8462cd0015262af
Dmitrii Liashko <mitya.lyashko.02@mail.ru>
2024-05-30 12:54:11 +0300

    Рефакторинг + CPoint

---
+++ LW_4/shapes/headers/CPoint.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <string>
+
+class Point
+{
+public:
+	std::string ToString() const;
+
+	double Distance(const Point& other) const;
+
+	bool operator==(const Point& other) const;
+
+	double x, y;
+};
