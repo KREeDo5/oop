c982a2f37a6dcdc2e44ff962da7ab2d0e5b3ef4e
Dmitrii Liashko <mitya.lyashko.02@mail.ru>
2024-06-05 21:20:51 +0300

    Отладка тестов + форматирование

--- LW_4/tests/Shapes.test.cpp
+++ LW_4/tests/Shapes.test.cpp
@@ -11,12 +11,12 @@ TEST_CASE("Тестирование классов фигур")
 	SECTION("Создание линии 5 6 9 1 00FF00")
 	{
 		CLineSegment line = CLineSegment(CPoint(5, 6), CPoint(9, 1), 16724991);
-		REQUIRE(line.GetArea() == 0);
+		REQUIRE(line.GetArea() == 0.0);
 		REQUIRE(fabs(line.GetPerimeter() - 6.4031242374) < DBL_EPSILON);
 		REQUIRE(line.GetStartPoint() == CPoint(5, 6));
 		REQUIRE(line.GetEndPoint() == CPoint(9, 1));
 		REQUIRE(line.GetOutlineColor() == 16724991);
-		REQUIRE(line.ToString() == "type: line segment\nleft start point: (5.00, 6.00)\nend point: (9.00, 1.00)\nlength: 6.40\noutline color : ff33ff\n");
+		REQUIRE(line.ToString() == TYPE_LINE + "\nleft start point: (5.00, 6.00)\nend point: (9.00, 1.00)\nlength: 6.40\noutline color : ff33ff\n");
 	}
 
 	SECTION("Создание круга 0 0 15 00FF33FF EEFFBBFF")
@@ -28,7 +28,7 @@ TEST_CASE("Тестирование классов фигур")
 		REQUIRE(circle.GetPerimeter() == 6.4031242374);
 		REQUIRE(circle.GetOutlineColor() == 16724991);
 		REQUIRE(circle.GetFillColor() == 4009737215);
-		REQUIRE(circle.ToString() == "type: circle\ncenter: (0.00, 0.00)\nradius: 15.00\narea: 706.86\nperimeter: 94.25\noutline color: ff33ff\nfill color: eeffbbff\n");
+		REQUIRE(circle.ToString() == TYPE_CIRCLE + "\ncenter: (0.00, 0.00)\nradius: 15.00\narea: 706.86\nperimeter: 94.25\noutline color: ff33ff\nfill color: eeffbbff\n");
 	}
 
 	SECTION("Создание прямоугольника 5 -6 10 10 00FF33FF EEFFBBFF")
@@ -42,7 +42,7 @@ TEST_CASE("Тестирование классов фигур")
 		REQUIRE(rectangle.GetRightBottom() == CPoint(rectangle.GetLeftTop().x + rectangle.GetWidth(), rectangle.GetLeftTop().y + rectangle.GetHeight()));
 		REQUIRE(rectangle.GetOutlineColor() == 16724991);
 		REQUIRE(rectangle.GetFillColor() == 4009737215);
-		REQUIRE(rectangle.ToString() == "type: rectangle\nleft top: (5.00, -6.00)\nwidth: 10.00\nheight: 10.00\narea: 100.00\nperimeter: 40.00\noutline color: ff33ff\nfill color: eeffbbff\n");
+		REQUIRE(rectangle.ToString() == TYPE_RECTANGLE + "\nleft top: (5.00, -6.00)\nwidth: 10.00\nheight: 10.00\narea: 100.00\nperimeter: 40.00\noutline color: ff33ff\nfill color: eeffbbff\n");
 	}
 
 	SECTION("Создание треугольника 0 0 3 4 6 0 00FF33FF EEFFBBFF")
@@ -55,7 +55,7 @@ TEST_CASE("Тестирование классов фигур")
 		REQUIRE(triangle.GetVertex3() == CPoint(6, 0));
 		REQUIRE(triangle.GetOutlineColor() == 16724991);
 		REQUIRE(triangle.GetFillColor() == 4009737215);
-		REQUIRE(triangle.ToString() == "type: triangle\nvertex 1: (0.00, 0.00)\nvertex 2: (3.00, 4.00)\nvertex 3: (6.00, 0.00)\narea: 12.00\nperimeter: 16.00\noutline color: ff33ff\nfill color: eeffbbff\n");
+		REQUIRE(triangle.ToString() == TYPE_TRIANGLE + "\nvertex 1: (0.00, 0.00)\nvertex 2: (3.00, 4.00)\nvertex 3: (6.00, 0.00)\narea: 12.00\nperimeter: 16.00\noutline color: ff33ff\nfill color: eeffbbff\n");
 	}
 }
 
@@ -74,31 +74,31 @@ TEST_CASE("ReadShapeData считывает данные из istream и сохраняет объект.")
 
 	SECTION("Обработка данных о линии")
 	{
-		std::istringstream iss("line 0 0 3 4 00FF00FF");
+		std::istringstream iss(LINE + " 0 0 3 4 00FF00FF");
 		REQUIRE(shapes.ReadShapeData(iss));
 	}
 
 	SECTION("Обработка некорректных данных о линии")
 	{
-		std::istringstream iss("line 0 0 3");
+		std::istringstream iss(LINE + " 0 0 3");
 		REQUIRE(!shapes.ReadShapeData(iss));
 	}
 
 	SECTION("Обработка данных треугольника")
 	{
-		std::istringstream iss("triangle 0 0 3 4 6 0 00FF00FF EEFFAAFF");
+		std::istringstream iss(TRIANGLE + " 0 0 3 4 6 0 00FF00FF EEFFAAFF");
 		REQUIRE(shapes.ReadShapeData(iss));
 	}
 
 	SECTION("Обработка данных прямоугольника")
 	{
-		std::istringstream iss("rectangle 0 10 20 10 00FF33FF EEFFBBFF");
+		std::istringstream iss(RECTANGLE + " 0 10 20 10 00FF33FF EEFFBBFF");
 		REQUIRE(shapes.ReadShapeData(iss));
 	}
 
 	SECTION("Обработка данных круга")
 	{
-		std::istringstream iss("circle 10 10 20 22AA33FF CCFFBBFF");
+		std::istringstream iss(CIRCLE + " 10 10 20 22AA33FF CCFFBBFF");
 		REQUIRE(shapes.ReadShapeData(iss));
 	}
 }
@@ -113,7 +113,7 @@ TEST_CASE("FindMaxAreaShape ищет фигуру максимальной площади.")
 	SECTION("Если нет фигур")
 	{
 		shapes.PrintMaxAreaShapeInfo(oss);
-		REQUIRE(oss.str() == "Failed to print shape data.\n");
+		REQUIRE(oss.str() == ERROR_PRINT_MAX_AREA + "\n");
 	}
 
 	SECTION("Если есть одна фигура")
@@ -121,7 +121,7 @@ TEST_CASE("FindMaxAreaShape ищет фигуру максимальной площади.")
 		std::istringstream iss("rectangle 0 10 20 10 00FF33FF EEFFBBFF");
 		REQUIRE(shapes.ReadShapeData(iss));
 		shapes.PrintMaxAreaShapeInfo(oss);
-		REQUIRE(oss.str() == "type: rectangle\nleft top: (0.00, 10.00)\nwidth: 20.00\nheight: 10.00\narea: 200.00\nperimeter: 60.00\noutline color: ff33ff\nfill color: eeffbbff\n");
+		REQUIRE(oss.str() == TYPE_RECTANGLE + "\nleft top: (0.00, 10.00)\nwidth: 20.00\nheight: 10.00\narea: 200.00\nperimeter: 60.00\noutline color: ff33ff\nfill color: eeffbbff\n");
 	}
 
 	SECTION("Находит прямоугольник максимальной площади: площадь первого прямоугольника = 50, второго = 100, третьего = 15")
@@ -129,7 +129,7 @@ TEST_CASE("FindMaxAreaShape ищет фигуру максимальной площади.")
 		std::istringstream iss("rectangle 1 3 10 5 00FF33FF EEFFBBFF\nrectangle 5 -6 10 10 00FF33FF EEFFBBFF\nrectangle -7 4 1 15 00FF33FF EEFFBBFF");
 		REQUIRE(shapes.ReadShapeData(iss));
 		shapes.PrintMaxAreaShapeInfo(oss);
-		REQUIRE(oss.str() == "type: rectangle\nleft top: (5.00, -6.00)\nwidth: 10.00\nheight: 10.00\narea: 100.00\nperimeter: 40.00\noutline color: ff33ff\nfill color: eeffbbff\n");
+		REQUIRE(oss.str() == TYPE_RECTANGLE + "\nleft top: (5.00, -6.00)\nwidth: 10.00\nheight: 10.00\narea: 100.00\nperimeter: 40.00\noutline color: ff33ff\nfill color: eeffbbff\n");
 	}
 
 	SECTION("Находит круг максимальной площади среди других типов фигур: площадь прямоугольника = 50, треугольника = 12, круга = 706,86.")
@@ -137,7 +137,7 @@ TEST_CASE("FindMaxAreaShape ищет фигуру максимальной площади.")
 		std::istringstream iss("rectangle 1 3 10 5 00FF33FF EEFFBBFF\ntriangle 0 0 3 4 6 0 00FF33FF EEFFBBFF\ncircle 0 0 15 00FF33FF EEFFBBFF");
 		REQUIRE(shapes.ReadShapeData(iss));
 		shapes.PrintMaxAreaShapeInfo(oss);
-		REQUIRE(oss.str() == "type: circle\ncenter: (0.00, 0.00)\nradius: 15.00\narea: 706.86\nperimeter: 94.25\noutline color: ff33ff\nfill color: eeffbbff\n");
+		REQUIRE(oss.str() == TYPE_CIRCLE + "\ncenter: (0.00, 0.00)\nradius: 15.00\narea: 706.86\nperimeter: 94.25\noutline color: ff33ff\nfill color: eeffbbff\n");
 	}
 }
 
@@ -151,7 +151,7 @@ TEST_CASE("FindMinPerimeterShape находит фигуру с минимальным периметром")
 	SECTION("Если нет фигур")
 	{
 		shapes.PrintMinPerimeterShapeInfo(oss);
-		REQUIRE(oss.str() == "Failed to print shape data.\n");
+		REQUIRE(oss.str() == ERROR_PRINT_MIN_PERIMETER + "\n");
 	}
 
 	SECTION("Если есть одна форма")
@@ -159,7 +159,7 @@ TEST_CASE("FindMinPerimeterShape находит фигуру с минимальным периметром")
 		std::istringstream iss("rectangle 0 10 20 10 00FF33FF EEFFBBFF");
 		REQUIRE(shapes.ReadShapeData(iss));
 		shapes.PrintMinPerimeterShapeInfo(oss);
-		REQUIRE(oss.str() == "type: rectangle\nleft top: (0.00, 10.00)\nwidth: 20.00\nheight: 10.00\narea: 200.00\nperimeter: 60.00\noutline color: ff33ff\nfill color: eeffbbff\n");
+		REQUIRE(oss.str() == TYPE_RECTANGLE + "\nleft top: (0.00, 10.00)\nwidth: 20.00\nheight: 10.00\narea: 200.00\nperimeter: 60.00\noutline color: ff33ff\nfill color: eeffbbff\n");
 	}
 
 	SECTION("Находит минимальный периметр прямоугольника: периметр первого прямоугольника = 30, второго = 40, третьего = 32.")
@@ -167,7 +167,7 @@ TEST_CASE("FindMinPerimeterShape находит фигуру с минимальным периметром")
 		std::istringstream iss("rectangle 1 3 10 5 00FF33FF EEFFBBFF\nrectangle 5 -6 10 10 00FF33FF EEFFBBFF\nrectangle -7 4 1 15 00FF33FF EEFFBBFF");
 		REQUIRE(shapes.ReadShapeData(iss));
 		shapes.PrintMinPerimeterShapeInfo(oss);
-		REQUIRE(oss.str() == "type: rectangle\nleft top: (1.00, 3.00)\nwidth: 10.00\nheight: 5.00\narea: 50.00\nperimeter: 30.00\noutline color: ff33ff\nfill color: eeffbbff\n");
+		REQUIRE(oss.str() == TYPE_RECTANGLE + "\nleft top: (1.00, 3.00)\nwidth: 10.00\nheight: 5.00\narea: 50.00\nperimeter: 30.00\noutline color: ff33ff\nfill color: eeffbbff\n");
 	}
 
 	SECTION("Находит треугольник с минимальным периметром среди других типов фигур: периметр прямоугольника = 30, второго = 16, круга = 94,25.")
@@ -175,6 +175,6 @@ TEST_CASE("FindMinPerimeterShape находит фигуру с минимальным периметром")
 		std::istringstream iss("rectangle 1 3 10 5 00FF33FF EEFFBBFF\ntriangle 0 0 3 4 6 0 00FF33FF EEFFBBFF\ncircle 0 0 15 00FF33FF EEFFBBFF");
 		REQUIRE(shapes.ReadShapeData(iss));
 		shapes.PrintMinPerimeterShapeInfo(oss);
-		REQUIRE(oss.str() == "type: triangle\nvertex 1: (0.00, 0.00)\nvertex 2: (3.00, 4.00)\nvertex 3: (6.00, 0.00)\narea: 12.00\nperimeter: 16.00\noutline color: ff33ff\nfill color: eeffbbff\n");
+		REQUIRE(oss.str() == TYPE_TRIANGLE + "\nvertex 1: (0.00, 0.00)\nvertex 2: (3.00, 4.00)\nvertex 3: (6.00, 0.00)\narea: 12.00\nperimeter: 16.00\noutline color: ff33ff\nfill color: eeffbbff\n");
 	}
 }
