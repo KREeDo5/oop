cd4f418797d3250329bec91f2e48872423e6dd79
Dmitrii Liashko <mitya.lyashko.02@mail.ru>
2024-06-12 12:39:50 +0300

    стандартные тесты для основных функций TODO: тесты для операторов

--- LW_5/ComplexTDD/Complex/test.cpp
+++ LW_5/ComplexTDD/Complex/test.cpp
@@ -3,13 +3,41 @@
 
 TEST_CASE("комплексные числа можно инициализировать и работать с ними", "[complex_class]")
 {
-	SECTION("инициализация и получение желаемых значений")
+	SECTION("инициализация c положит.значениями")
 	{	
 		Complex complex(3, 4);													 //3 + 4i
+
 		REQUIRE(complex.Re() == 3.0);											 //возврат действительной части комплексного числа
 		REQUIRE(complex.Im() == 4.0);											 //возвращает мнимую часть комплексного числа
 
 		REQUIRE(complex.GetArgument() == std::tan(complex.Im() / complex.Re())); //возвращает аргумент комплексного числа
+
 		REQUIRE(complex.GetMagnitude() == 5.0);									 //возвращает модуль комплексного числа
 	}
+
+	SECTION("инициализация c отриц.значениями")
+	{
+		Complex complex(-3, -4);
+
+		REQUIRE(complex.Re() == -3.0);
+		REQUIRE(complex.Im() ==- 4.0);
+
+		REQUIRE(complex.GetArgument() == std::tan(complex.Im() / complex.Re()));
+
+		REQUIRE(complex.GetMagnitude() == 5.0);
+	}
+
+	SECTION("инициализация c различными значениями")
+	{
+		Complex complex(3, -4);
+
+		REQUIRE(complex.Re() == 3.0);
+		REQUIRE(complex.Im() == -4.0);
+
+		REQUIRE(complex.GetArgument() == std::tan(complex.Im() / complex.Re()));
+
+		REQUIRE(complex.GetMagnitude() == 5.0);
+
+	}
+
 }
