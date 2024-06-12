0f7ae7619b85feb97433f0434cb12dcfb0a2a650
Dmitrii Liashko <mitya.lyashko.02@mail.ru>
2024-06-12 12:57:02 +0300

    1.1 Тест на оператор суммы комплексных чисел (бинарный +) 1.2 Тест суммы действительного и комплексного числа

--- LW_5/ComplexTDD/Complex/test.cpp
+++ LW_5/ComplexTDD/Complex/test.cpp
@@ -1,9 +1,9 @@
 #define CATCH_CONFIG_MAIN
 #include "D:/Repos/VOLGATECH_WORK/OOP/LW_5/ComplexTDD/catch2/catch.hpp"
 
-TEST_CASE("комплексные числа можно инициализировать и работать с ними", "[complex_class]")
+TEST_CASE("Комплексные числа можно инициализировать и работать с ними")
 {
-	SECTION("инициализация c положит.значениями")
+	SECTION("Инициализация c положит.значениями")
 	{	
 		Complex complex(3, 4);													 //3 + 4i
 
@@ -15,7 +15,7 @@ TEST_CASE("комплексные числа можно инициализировать и работать с ними", "[comple
 		REQUIRE(complex.GetMagnitude() == 5.0);									 //возвращает модуль комплексного числа
 	}
 
-	SECTION("инициализация c отриц.значениями")
+	SECTION("Инициализация c отриц.значениями")
 	{
 		Complex complex(-3, -4);
 
@@ -27,7 +27,7 @@ TEST_CASE("комплексные числа можно инициализировать и работать с ними", "[comple
 		REQUIRE(complex.GetMagnitude() == 5.0);
 	}
 
-	SECTION("инициализация c различными значениями")
+	SECTION("Инициализация c различными значениями")
 	{
 		Complex complex(3, -4);
 
@@ -40,4 +40,67 @@ TEST_CASE("комплексные числа можно инициализировать и работать с ними", "[comple
 
 	}
 
+}
+
+
+TEST_CASE("1.1) Сумма комплексных чисел")
+{
+	SECTION("Сумма двух комплексных чисел с положительными знаками")
+	{
+		Complex complex1(12, 34);
+		Complex complex2(56, 78);
+
+		auto complexResult = complex1 + complex2;
+
+		REQUIRE(complexResult.Re() == 68.0);  // 12 + 56 = 68
+		REQUIRE(complexResult.Im() == 112.0); // 34 + 78 = 112
+	}
+
+
+	SECTION("Сумма двух комплексных чисел с отрицательными знаками")
+	{
+		Complex complex1(-12, -34);
+		Complex complex2(-56, -78);
+
+		auto complexResult = complex1 + complex2;
+
+		REQUIRE(complexResult.Re() == -68.0);  // -12 - 56 = -68
+		REQUIRE(complexResult.Im() == -112.0); // -34 - 78 = -112
+	}
+
+	SECTION("Сумма двух комплексных чисел с различными знаками")
+	{
+		Complex complex1(12, -34);
+		Complex complex2(-56, 78);
+
+		auto complexResult = complex1 + complex2;
+
+		REQUIRE(complexResult.Re() == -44.0); // 12 - 56 = -(56 - 12) = -44 
+		REQUIRE(complexResult.Im() == 44.0);  // -34 + 78 = 44
+	}
+}
+
+TEST_CASE("1.2) Сумма комплексного и действительного числа")
+{
+	SECTION("Сумма комплексного числа с положительным действительным")
+	{
+		Complex complex(12, -34);
+		double real = 56.0;
+
+		auto complexResult = complex + real;
+
+		REQUIRE(complexResult.Re() == 68.0);
+		REQUIRE(complexResult.Im() == -34.0);
+	}
+
+	SECTION("Сумма комплексного числа с отрицательным действительным")
+	{
+		Complex complex(12, -34);
+		double real = -56.0;
+
+		auto complexResult = complex + real;
+
+		REQUIRE(complexResult.Re() == -44.0);
+		REQUIRE(complexResult.Im() == -34.0);
+	}
 }
