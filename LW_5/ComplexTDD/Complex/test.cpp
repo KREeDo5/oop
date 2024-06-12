4689e8b00feac22195f535f57e8509f6bf984080
Dmitrii Liashko <mitya.lyashko.02@mail.ru>
2024-06-12 12:33:32 +0300

    первый тест

---
+++ LW_5/ComplexTDD/Complex/test.cpp
@@ -0,0 +1,15 @@
+#define CATCH_CONFIG_MAIN
+#include "D:/Repos/VOLGATECH_WORK/OOP/LW_5/ComplexTDD/catch2/catch.hpp"
+
+TEST_CASE("комплексные числа можно инициализировать и работать с ними", "[complex_class]")
+{
+	SECTION("инициализация и получение желаемых значений")
+	{	
+		Complex complex(3, 4);													 //3 + 4i
+		REQUIRE(complex.Re() == 3.0);											 //возврат действительной части комплексного числа
+		REQUIRE(complex.Im() == 4.0);											 //возвращает мнимую часть комплексного числа
+
+		REQUIRE(complex.GetArgument() == std::tan(complex.Im() / complex.Re())); //возвращает аргумент комплексного числа
+		REQUIRE(complex.GetMagnitude() == 5.0);									 //возвращает модуль комплексного числа
+	}
+}
